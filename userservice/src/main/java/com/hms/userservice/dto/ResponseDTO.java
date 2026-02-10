469ef66981e8fabeb3759f3257bdadf306181106
Sneha <sneharoy90737@gmail.com>
2026-02-10 20:38:30 +0530

    Login Response DTO add

---
+++ userservice/src/main/java/com/hms/userservice/dto/ResponseDTO.java
@@ -0,0 +1,12 @@
+package com.hms.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO {
+  private String message;
+}
